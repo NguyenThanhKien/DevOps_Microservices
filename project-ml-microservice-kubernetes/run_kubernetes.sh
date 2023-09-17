138199eb8f34aa6868f09929dba1fdd3806ff47b
KienNguyen <thanhkien0302@gmail.com>
2023-09-17 14:38:46 +0700

    update code

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=kiennt65/b3cfa98054a7
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run app --image=$dockerpath:version1.0 --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward app 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=kiennt65/b3cfa98054a7
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+echo "Docker ID and Image: $dockerpath"
+docker login -u kiennt65
+docker tag api $dockerpath:version1.0
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:version1.0
